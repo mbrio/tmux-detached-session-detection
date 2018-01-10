50fff5b611ea60ca71c2eaf7b479db2b72a3ae21
Michael Diolosa <michael.diolosa@gmail.com>
2018-01-10 09:57:47 -0500

    Change message for tasd

--- tasd.sh
+++ tasd.sh
@@ -2,7 +2,7 @@
 
 tput setaf 0;
 tput setab 10;
-printf $"\n\uE0B0 There are detached tmux sessions on this machine.\x1B[K";
+printf $"\n\uE0B0 All tmux sessions on this machine.\x1B[K";
 tput sgr0;
 printf "\n";
 tmux ls
